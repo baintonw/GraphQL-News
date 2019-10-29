5c8d64ed541ff62bfb7fc2e58651535cbe8eb4c9
Will Bainton <baintonw@gmail.com>
2019-10-29 15:32:48 -0400

    added query Component from apollo-react

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../styles/App.css';
+
+import LinkList from './LinkList.js'
+
+function App() {
+  return (
+    <div className="App">
+      <LinkList />
+    </div>
+  );
+}
+
+export default App;
