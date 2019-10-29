0e4998fdd9c4eec52de54244342f1a93fe12fbd3
Will Bainton <baintonw@gmail.com>
2019-10-29 15:47:38 -0400

    added Muation component to CreateLink component, and implemented post functionality

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from '../logo.svg';
 import '../styles/App.css';
 
+import CreateLink from './CreateLink'
+
 import LinkList from './LinkList.js'
 
 function App() {
   return (
     <div className="App">
-      <LinkList />
+      <CreateLink />
     </div>
   );
 }
