0caa831949eee0389c51dd47b8013652f345aacf
Will Bainton <baintonw@gmail.com>
2019-10-31 19:40:29 -0400

    added top, prevpage, nextpage functionality

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import logo from '../logo.svg';
 import '../styles/App.css';
 
 import Header from './Header'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 import CreateLink from './CreateLink'
 import LinkList from './LinkList.js'
@@ -16,10 +16,13 @@ function App() {
       <Header />
       <div className="ph3 pv1 background-gray">
         <Switch>
+          <Route exact path='/' render={() => <Redirect to='/new/1' />} />
           <Route exact path="/" component={LinkList} />
           <Route exact path="/create" component={CreateLink} />
           <Route exact path="/login" component={Login} />
           <Route exact path='/search' component={Search} />
+          <Route exact path='/top' component={LinkList} />
+          <Route exact path='/new/:page' component={LinkList} />
         </Switch>
       </div>
     </div>
